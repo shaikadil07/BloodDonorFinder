fd19b8161ceff16f59dbb32c98bd2e569ffec6e4
shaikadil07 <adilshaik3690@gmail.com>
2026-07-12 13:20:21 +0530

    Fixed user auto id generation

--- src/main/java/blooddonor/User.java
+++ src/main/java/blooddonor/User.java
@@ -1,13 +1,19 @@
 package blooddonor;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name="USERS")
 public class User {
 
     @Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
     private Long id;
 
     @Column(name="NAME")
